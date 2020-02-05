840e459da8235f9fa74220a9ec37354ad61e04a6
Davi <davi_pexoto@hotmail.com>
2020-02-05 10:34:42 -0300

    Fixed: Models

--- db/models.js
+++ db/models.js
@@ -1,63 +1,10 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-    users: mongoose.model('Users', {
+    user: mongoose.model('User', {
         name: String,
         username: String,
         password: String,
         created_on: { type : Date, default: Date.now }
-    }),
-
-    dates: mongoose.model('Dates', {
-        title: String,
-        date: String,
-        content: String,
-        year: { type: Number, min: 2, max: 4 },
-        month: { type: Number, min: 1, max: 2 },
-        day: { type: Number, min: 1, max: 2 },
-        created_on: { type : Date, default: Date.now }
-    }),
-
-    news: mongoose.model('News', {
-        title: String,
-        date: String,
-        content: String,
-        year: { type: Number, min: 2, max: 4 },
-        month: { type: Number, min: 1, max: 2 },
-        day: { type: Number, min: 1, max: 2 },
-        created_on: { type : Date, default: Date.now }
-    }),
-
-    estudies: mongoose.model('Estudies', {
-        title: String,
-        date: String,
-        content: String,
-        year: { type: Number, min: 2, max: 4 },
-        month: { type: Number, min: 1, max: 2 },
-        day: { type: Number, min: 1, max: 2 },
-        created_on: { type : Date, default: Date.now }
-    }),
-
-    cenaculos: mongoose.model('Cenaculos', {
-        title: String,
-        date: String,
-        content: String,
-        year: { type: Number, min: 2, max: 4 },
-        month: { type: Number, min: 1, max: 2 },
-        day: { type: Number, min: 1, max: 2 },
-        created_on: { type : Date, default: Date.now }
-    }),
-
-    prays: mongoose.model('Prays', {
-        title: String,
-        name: { type: String, default: "Anônimo"},
-        date: String,
-        content: String,
-        coments: Array,
-        authorized: Boolean,
-        year: { type: Number, min: 2, max: 4 },
-        month: { type: Number, min: 1, max: 2 },
-        day: { type: Number, min: 1, max: 2 },
-        created_on: { type : Date, default: Date.now }
-    }),
+    })
 }
